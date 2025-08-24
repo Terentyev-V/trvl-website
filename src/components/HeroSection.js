709872c7efb0f5af8d42fed57dd808de303fe8a7
BlackT <terentyevBlackthorn@gmail.com>
2025-08-24 12:40:41 +0200

    Initial commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  const [videoError, setVideoError] = useState(false);
+
+  return (
+    <div className='hero-container'>
+      {!videoError ? (
+        <video 
+          src="/videos/video-2.mp4" 
+          autoPlay 
+          loop 
+          muted 
+          poster="/images/img-home.jpg"
+          onError={() => setVideoError(true)}   // если ошибка загрузки → ставим картинку
+        />
+      ) : (
+        <img 
+          src="/images/img-home.jpg" 
+          alt="Background" 
+          className="fallback-img"
+        />
+      )}
+
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>  
+      <div className='hero-btns'>
+        <Button 
+          className='btns' 
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+
+        <Button 
+          className='btns' 
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>  
+    </div>
+  )
+}
+
+export default HeroSection;
